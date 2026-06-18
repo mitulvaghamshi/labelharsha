09de633841b9040ee17ef46a217caae1afcdc2e9
mitulvaghamshi <mitulvaghmashi@gmail.com>
2026-06-18 17:38:35 +0530

    Simplify product categories; Update relevent css

--- src/utils/category-list.js
+++ src/utils/category-list.js
@@ -1,19 +1,8 @@
 export const CATEGORIES = [
-    { id: "all", label: "All Styles" },
-    { id: "single-kurta", label: "Single Kurta" },
-    { id: "tunic-kaftan", label: "Tunic Kaftan" },
-    { id: "kurta-set", label: "Kurta Set" },
-    { id: "co-ord-set", label: "Co-Ord Set" },
-    { id: "tunic-set", label: "Tunic Set" },
-    { id: "gown-style", label: "Gown Style" },
-    { id: "silk-kurta-set", label: "Silk Kurta Set" },
-    { id: "kurta-suit-set", label: "Kurta Suit Set" },
-    { id: "anarkali-set", label: "Anarkali Set" },
-    { id: "suit-set", label: "Suit Set" },
-    { id: "salwar-kameez", label: "Salwar Kameez" },
-    { id: "silk-kurta", label: "Silk Kurta" },
-    { id: "short-kurta", label: "Short Kurta" },
-    { id: "sharara-set", label: "Sharara Set" },
-    { id: "straight-kurta", label: "Straight Kurta" },
-    { id: "kurta-palazzo-set", label: "Kurta Palazzo Set" }
+    { id: "all", label: "All" },
+    { id: "kurta", label: "Kurta" },
+    { id: "kurta-sets", label: "Kurta & Suit Sets" },
+    { id: "tunic-kaftan", label: "Tunic & Kaftans" },
+    { id: "ethnic-fusion", label: "Co-Ord & Fusion" },
+    { id: "gown-anarkali", label: "Gown & Anarkali" }
 ];
